dc79e9c50034b72718b8ef49d95aa0391e490e09
BatatzxX <117692543+BatatzxX@users.noreply.github.com>
2022-12-03 10:40:36 -0300

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,27 @@
+echo: WARNING
+echo: HI! THIS IS A MAWARE.
+echo: IF YOU DONT WANT RUN
+echo: PLEASE CLOSE THIS WINDOW
+echo: OR END THIS TASK W/ TASK MANANGER.
+pause
+echo: LAST WARNING
+echo: THIS IS NOT A JOKE,
+echo: IF YOU WANT YOUR MACHINE INFECTED,
+echo: RUN THIS, I WILL NOT BR BLAMED!
+pause
+msg * welcome to hell
+echo MsgBox('.') > a.vbs
+echo MsgBox('y') > a.vbs
+start a.vbs
+start a.vbs
+start a.vbs
+del a.vbs
+start https://github.com/BatatzxX/Trojan.Win32-Torture.A/
+start https://github.com/BatatzxX/Trojan.Win32-Torture.A/
+start https://github.com/BatatzxX/Trojan.Win32-Torture.A/
+start https://github.com/BatatzxX/Trojan.Win32-Torture.A/
+echo msg * HAHAHAHAAHAH > msg.bat
+echo msg * still you using this computer? > msg.bat
+start msg.bat
+start msg.bat
+start msg.bat
